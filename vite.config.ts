d7901d321f06057cf57826a76691878489d9fcc9
kss2002 <kss2002@users.noreply.github.com>
2026-01-14 17:38:16 +0900

    프록시 설정

--- vite.config.ts
+++ vite.config.ts
@@ -6,6 +6,16 @@ import tailwindcss from '@tailwindcss/vite';
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [tailwindcss(), react()],
+  server: {
+    proxy: {
+      '/api': {
+        target: 'https://jungjung-backend.oursophy.com',
+        changeOrigin: true,
+        secure: false,
+        rewrite: (path) => path, // /api/search -> /api/search 그대로 전달
+      },
+    },
+  },
   resolve: {
     alias: {
       '@': path.resolve(__dirname, './src'),
